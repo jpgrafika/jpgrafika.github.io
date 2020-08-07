a22faeae12fbc77dc4e0f4fd0c0262425fe45390
jpgrafika <j.piotrowskagrafika@gmail.com>
2020-08-07 14:53:51 +0200

    improvement to admin

--- admin.py
+++ admin.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import webbrowser
 
 import PySimpleGUI as gui
 import psutil as psutil
@@ -11,10 +12,12 @@ class GitHubPagesProject:
         self.window = None
         self.test_process = None
         self.deploy_process = None
+        self.path = 'jpgrafika.github.io'
 
     def event_handler(self, event_key, values):
         if event_key.startswith('test'):
-            self.test_process = subprocess.Popen('exec ./test.sh', shell=True)
+            self.test_process = subprocess.Popen(f'exec ./test.sh', shell=True, cwd=self.path)
+            webbrowser.open('http://0.0.0.0:8000')
         elif event_key.startswith('stop'):
             if self.test_process:
                 self.kill_process(self.test_process)
@@ -24,14 +27,14 @@ class GitHubPagesProject:
                 print('wasn\'t running')
         elif event_key.startswith('download'):
             password = values.get('pass')
-            if os.path.exists('index.html'):
-                os.system('git pull')
+            if os.path.exists(self.path):
+                os.system(f'git -C {self.path} pull')
                 print('updated')
             else:
-                os.system(f'git clone https://jpgrafika:{password}github.com/jpgrafika/jpgrafika.github.io.git')
+                os.system(f'git clone https://jpgrafika:{password}@github.com/jpgrafika/jpgrafika.github.io.git')
                 print('downloaded')
         elif event_key.startswith('deploy'):
-            self.deploy_process = subprocess.Popen('exec ./deploy.sh', shell=True)
+            self.deploy_process = subprocess.Popen(f'exec ./deploy.sh', shell=True, cwd=self.path)
         else:
             self.show_error('Unknown command: {}'.format(event_key))
 
